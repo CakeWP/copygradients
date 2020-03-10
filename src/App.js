295ed720ef6c26a054421fb44f8ba6097c477170
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2020-03-10 13:28:49 +0800

    Install packages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="flex flex-wrap -mx-5 overflow-hidden sm:-mx-4 md:-mx-4">
+        <div class="my-5 px-5 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-4 md:px-4 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit habitant
+          quisque; sed vulputate eros ex. Curabitur nostra nisi dis diam
+          penatibus metus proin phasellus. Turpis ullamcorper ante ex ipsum
+          ligula; auctor phasellus lorem. Vestibulum erat suspendisse nec netus
+          leo nisl habitasse rhoncus. Adipiscing quam at scelerisque metus magna
+          primis congue.
+        </div>
+
+        <div class="my-5 px-5 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-4 md:px-4 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit habitant
+          quisque; sed vulputate eros ex. Curabitur nostra nisi dis diam
+          penatibus metus proin phasellus. Turpis ullamcorper ante ex ipsum
+          ligula; auctor phasellus lorem. Vestibulum erat suspendisse nec netus
+          leo nisl habitasse rhoncus. Adipiscing quam at scelerisque metus magna
+          primis congue.
+        </div>
+
+        <div class="my-5 px-5 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-4 md:px-4 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit habitant
+          quisque; sed vulputate eros ex. Curabitur nostra nisi dis diam
+          penatibus metus proin phasellus. Turpis ullamcorper ante ex ipsum
+          ligula; auctor phasellus lorem. Vestibulum erat suspendisse nec netus
+          leo nisl habitasse rhoncus. Adipiscing quam at scelerisque metus magna
+          primis congue.
+        </div>
+
+        <div class="my-5 px-5 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-4 md:px-4 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit habitant
+          quisque; sed vulputate eros ex. Curabitur nostra nisi dis diam
+          penatibus metus proin phasellus. Turpis ullamcorper ante ex ipsum
+          ligula; auctor phasellus lorem. Vestibulum erat suspendisse nec netus
+          leo nisl habitasse rhoncus. Adipiscing quam at scelerisque metus magna
+          primis congue.
+        </div>
+
+        <div class="my-5 px-5 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-4 md:px-4 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit habitant
+          quisque; sed vulputate eros ex. Curabitur nostra nisi dis diam
+          penatibus metus proin phasellus. Turpis ullamcorper ante ex ipsum
+          ligula; auctor phasellus lorem. Vestibulum erat suspendisse nec netus
+          leo nisl habitasse rhoncus. Adipiscing quam at scelerisque metus magna
+          primis congue.
+        </div>
+
+        <div class="my-5 px-5 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-4 md:px-4 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit habitant
+          quisque; sed vulputate eros ex. Curabitur nostra nisi dis diam
+          penatibus metus proin phasellus. Turpis ullamcorper ante ex ipsum
+          ligula; auctor phasellus lorem. Vestibulum erat suspendisse nec netus
+          leo nisl habitasse rhoncus. Adipiscing quam at scelerisque metus magna
+          primis congue.
+        </div>
+
+        <div class="my-5 px-5 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-4 md:px-4 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit habitant
+          quisque; sed vulputate eros ex. Curabitur nostra nisi dis diam
+          penatibus metus proin phasellus. Turpis ullamcorper ante ex ipsum
+          ligula; auctor phasellus lorem. Vestibulum erat suspendisse nec netus
+          leo nisl habitasse rhoncus. Adipiscing quam at scelerisque metus magna
+          primis congue.
+        </div>
+
+        <div class="my-5 px-5 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-4 md:px-4 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit habitant
+          quisque; sed vulputate eros ex. Curabitur nostra nisi dis diam
+          penatibus metus proin phasellus. Turpis ullamcorper ante ex ipsum
+          ligula; auctor phasellus lorem. Vestibulum erat suspendisse nec netus
+          leo nisl habitasse rhoncus. Adipiscing quam at scelerisque metus magna
+          primis congue.
+        </div>
+
+        <div class="my-5 px-5 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-4 md:px-4 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit habitant
+          quisque; sed vulputate eros ex. Curabitur nostra nisi dis diam
+          penatibus metus proin phasellus. Turpis ullamcorper ante ex ipsum
+          ligula; auctor phasellus lorem. Vestibulum erat suspendisse nec netus
+          leo nisl habitasse rhoncus. Adipiscing quam at scelerisque metus magna
+          primis congue.
+        </div>
+
+        <div class="my-5 px-5 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-4 md:px-4 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit habitant
+          quisque; sed vulputate eros ex. Curabitur nostra nisi dis diam
+          penatibus metus proin phasellus. Turpis ullamcorper ante ex ipsum
+          ligula; auctor phasellus lorem. Vestibulum erat suspendisse nec netus
+          leo nisl habitasse rhoncus. Adipiscing quam at scelerisque metus magna
+          primis congue.
+        </div>
+
+        <div class="my-5 px-5 w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-4 md:px-4 md:w-1/4 lg:w-1/5 xl:w-1/6">
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Suscipit habitant
+          quisque; sed vulputate eros ex. Curabitur nostra nisi dis diam
+          penatibus metus proin phasellus. Turpis ullamcorper ante ex ipsum
+          ligula; auctor phasellus lorem. Vestibulum erat suspendisse nec netus
+          leo nisl habitasse rhoncus. Adipiscing quam at scelerisque metus magna
+          primis congue.
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
