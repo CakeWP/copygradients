9ef198a82e146f0f45b5037f5c806bbfe96f4abb
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2020-03-10 14:08:08 +0800

    Add GradientList component

---
+++ src/components/gradients-list/index.js
@@ -0,0 +1,20 @@
+/**
+ * External dependencies
+ */
+import React, { Component, Fragment } from "react";
+import { map } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import gradients from './gradients.json';
+
+class Charmap extends Component {
+  constructor() {
+    super(...arguments);
+  }
+
+  render() {}
+}
+
+export default Charmap;
