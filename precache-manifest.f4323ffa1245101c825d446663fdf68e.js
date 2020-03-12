5feb332751860635fb3e900bbdd1c325785c1795
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2020-03-12 12:48:05 +0800

    Updates

---
+++ precache-manifest.f4323ffa1245101c825d446663fdf68e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358cafc0826d1fefa1682ef6677fddf2",
+    "url": "/copygradients/index.html"
+  },
+  {
+    "revision": "5b8340d49e8ec2afc44a",
+    "url": "/copygradients/static/css/main.7d3a82d8.chunk.css"
+  },
+  {
+    "revision": "e6e5e2cd06db22c53e3a",
+    "url": "/copygradients/static/js/2.bebbd84d.chunk.js"
+  },
+  {
+    "revision": "03fe4d632d4c060cb7cfad7727730c86",
+    "url": "/copygradients/static/js/2.bebbd84d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8340d49e8ec2afc44a",
+    "url": "/copygradients/static/js/main.83125389.chunk.js"
+  },
+  {
+    "revision": "43fba4ce03ca4f2f56f8",
+    "url": "/copygradients/static/js/runtime-main.b512c897.js"
+  }
+]);
